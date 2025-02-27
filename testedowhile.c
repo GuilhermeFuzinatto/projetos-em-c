0a9f7753b617d69e59c3cc19fe23df2ff69289a2
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-02-27 09:20:38 -0300

    Add files via upload

--- testedowhile.c
+++ testedowhile.c
@@ -5,15 +5,24 @@ int main(){
     int a;
     int b;
     int soma;
-    char d;
+    char d1;
+    char d2;
     do{
-        printf("digite dois números: ");
-        scanf("%d %d", &a, &b);
-        soma = a + b;
-        printf("a soma dos dois é %d", soma);
-        printf("\ndeseja fazer novamente? (s pra sim, n pra nao): ");
-        scanf(" %c", &d);
-    }while(d=='s');
+        printf("abrir menu? (s para sim, n para nao): ");
+        scanf(" %c", &d1);
+        switch(d1){
+            case 's':
+                do{
+                    printf("digite dois números: ");
+                    scanf("%d %d", &a, &b);
+                    soma = a + b;
+                    printf("a soma dos dois é %d", soma);
+                    printf("\ndeseja fazer novamente? (s pra sim, n pra nao): ");
+                    scanf(" %c", &d2);
+                }while(d2=='s');
+        }
+    }while(d1=='s');
+    printf("fim");
 
     return 0;
 }
